2285e0c59d4e40e441ab9b8a71d51bd96cf7a3cb
meghamohan <meghamohan1989@gmail.com>
2017-06-08 07:03:55 +0000

    fixed issues with init and basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -3,16 +3,28 @@
 
 from datetime import datetime, date, time
 from uuid import uuid4
+import json
+from models import storage
 
 class BaseModel:
     """ defining BaseModel class """
     def __init__(self, *args, **kwargs):
-        self.id = str(uuid4())
-        self.created_at = datetime.now()
-        self.update_at = datetime.now()
+        if len(args) > 0 and type(args[0]) is dict:
+            if 'id' in kwargs:
+                self.id = kwargs.get('id')
+            if 'created_at' in kwargs:
+                self.created_at = datetime.strptime(kwargs.get('created_at'), "%Y-%m-%d %H:%M:%S.%f")
+            if 'updated_at' in kwargs:
+                self.updated_at = datetime.strptime(kwargs.get('updated_at'), "%Y-%m-%d %H:%M:%S.%f")
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_json(self):
         newDict = self.__dict__.copy()
