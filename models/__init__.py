501f4a798ceb156f5e07a6e961eb27ada0633b82
meghamohan <meghamohan1989@gmail.com>
2017-06-12 19:18:15 +0000

    pep8 fixes

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
+#!/usr/bin/python3
+""" Module for init"""
+
+
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Base model class """
+
+
 from datetime import datetime, date, time
 from uuid import uuid4
 import json
